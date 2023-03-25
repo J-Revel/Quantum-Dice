3d32d90ed9730e2771f731ea6a087e6a68345809
J-Revel <j.revel@protonmail.com>
2023-03-25 10:34:41 +0100

    Added basic level config

--- Project/Assets/Scripts/Gameplay/DiceManager.cs
+++ Project/Assets/Scripts/Gameplay/DiceManager.cs
@@ -24,10 +24,17 @@ public struct IntricationGroup
 
 public class DiceManager : MonoBehaviour
 {
+    public LevelConfig config;
     public DiceState[] dice;
     public IntricationGroup[] intricationGroups;
     public System.Action diceRollDelegate;
 
+    public void Start()
+    {
+        dice = new DiceState[config.diceCount];
+        for(int i=0; i<config.diceCount; i++)
+            dice[i].value = 0;
+    }
     public void RollDice(int diceIndex)
     {
         dice[diceIndex].value = Random.Range(1, 7);
---
+++ Project/Assets/Scripts/Gameplay/LevelConfig.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class LevelConfig : ScriptableObject
+{
+    public int diceCount = 5;
+}
