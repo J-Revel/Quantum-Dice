bc3131ecd1227194776b9eaae0f50a2eb98cb5eb
lamouchiambre <lamouchiambre@gmail.com>
2023-03-25 11:53:33 +0100

    add RollDice

--- Project/Assets/Scripts/Gameplay/DiceManager.cs
+++ Project/Assets/Scripts/Gameplay/DiceManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [System.Serializable]
@@ -19,7 +21,7 @@ public enum IntricationMode
 public struct IntricationGroup
 {
     public IntricationMode mode;
-    int[] diceIndex;
+    public int[] diceIndex;
 }
 
 public class DiceManager : MonoBehaviour
@@ -37,7 +39,53 @@ public class DiceManager : MonoBehaviour
     }
     public void RollDice(int diceIndex)
     {
-        dice[diceIndex].value = Random.Range(1, 7);
+        //liste des valeurs possible
+        int[] valuesPossible = { 1, 2, 3, 4, 5, 6 };
+        
+        //parcourir l'ensemble des groupe d'intrication
+        for (int i = 0; i < intricationGroups.Length; i++ )
+        {
+            int[] dicesIndex = intricationGroups[i].diceIndex;
+
+            //verifier s'il est contenue dans la liste diceIndex
+
+            if (Array.IndexOf(dicesIndex, diceIndex) != -1)
+            {
+
+                int[] dicesIndexCopy = dicesIndex.Except(new int[] { diceIndex }).ToArray();
+
+                if (intricationGroups[i].mode == IntricationMode.Selfish)
+                {
+                    for (int k = 0; k < dicesIndexCopy.Length; k++)
+                    {
+                        valuesPossible = valuesPossible.Except(new int[] { k }).ToArray();
+                    }
+                }
+
+                if (intricationGroups[i].mode == IntricationMode.Gregarious)
+                {
+                    for (int k = 0; k < dicesIndexCopy.Length; k++) // a achanger pour une boucle while
+                    {
+                        if(dicesIndexCopy[k] != 0)
+                        {
+                            dice[diceIndex].value = dicesIndexCopy[k];
+                        }
+                    }
+                }
+                if (intricationGroups[i].mode == IntricationMode.Opposite)
+                {
+                    for (int k = 0; k < dicesIndexCopy.Length; k++)
+                    {
+                        int tmp = 7 - dicesIndexCopy[k];
+                        //valuesPossible = {tmp};
+                    }
+                }
+            }
+        }
+
+
+        //dice[diceIndex].value = System.Random.Range(1, 7);
+
         diceRollDelegate?.Invoke();
     }
 }
